58098b59db1cb339b9c2f089db6c2a95d37c113b
ShogunPanda <shogun_panda@me.com>
2013-08-18 21:43:19 -0700

    Fixed CLI.

--- lib/devdnsd/version.rb
+++ lib/devdnsd/version.rb
@@ -16,7 +16,7 @@ module DevDNSd
     MINOR = 0
 
     # The patch version.
-    PATCH = 1
+    PATCH = 2
 
     # The current version number of DevDNSd.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
